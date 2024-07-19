96c4fbcbb1497481a3d49d2fb3e0cf66bf60c679
D1S01 <diegosg513@gmail.com>
2024-07-19 12:00:07 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+/** Componente para preparar render en index */
+// import { MyPlaylist } from "./components/MyPlaylist"
+
+function App(){
+    // return <MyPlaylist />
+    console.log('saludando desde consola')
+
+    return (
+        <div className="container">
+            <h1>This is a a testing title</h1>
+            <p>
+                Until i figure out how to do This, im gonna be testing in this page
+            </p>
+            <form action="">
+                <label htmlFor="texto">is this a text input?: <input id="texto" type="text" /></label>
+                <input type="checkbox" /> yes it is! <input type="checkbox" /> no it isn't!
+            </form>
+        </div>
+    )
+}
+
+export { App };
